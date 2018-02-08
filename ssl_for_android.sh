8b62e2985446421594273101fb28f7933205c1f1
alanpoon <brightshadowx@gmail.com>
2018-02-08 12:32:16 +0800

    d

--- ssl_for_android.sh
+++ ssl_for_android.sh
@@ -77,7 +77,7 @@ make -s && make install
 pwd
 ls
 
-cp -f "Android.mk" "${OUTPUT_DIR}/Android.mk"
+cp -f "${NDK_HOME}/Android.mk" "${OUTPUT_DIR}/Android.mk"
 rm -rf "${NDK_TOOLCHAIN_PATH}"
 
 export OPENSSL_DIR="/usr/local/ssl/android-21/armeabi-v7a"

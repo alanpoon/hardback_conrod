cc77e7574fc5dc124d50e57a2b91bd2ffae7da0f
alanpoon <brightshadowx@gmail.com>
2018-05-06 12:27:42 +0800

    t18

--- .circleci/upload_github_release.sh
+++ .circleci/upload_github_release.sh
@@ -11,7 +11,7 @@ owner=$CIRCLE_PROJECT_USERNAME
 repo=$CIRCLE_PROJECT_REPONAME
 filename=./hardback-debug.apk
 GH_REPO="https://api.github.com/repos/$owner/$repo"
-GH_TAGS="$GH_REPO/releases/tags/$tag"
+GH_TAGS="$GH_REPO/releases/latest"
 AUTH="Authorization: token $GITHUB_API_TOKEN"
 
 cp ./target/android-artifacts/app/build/outputs/apk/app-debug.apk $filename
@@ -21,14 +21,15 @@ curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or 
 
 # Create a release
 RELEASE_URL="https://api.github.com/repos/$owner/$repo/releases?access_token=$GITHUB_API_TOKEN"
-curl --data "{\"tag_name\": \"$tag\"}" $RELEASE_URL
+#curl --data "{\"tag_name\": \"$tag\"}" $RELEASE_URL
+curl $RELEASE_URL
 
 # Read asset tags
 response=$(curl -sH "$AUTH" $GH_TAGS)
 
 # Get ID of the asset based on given filename
 eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response\n" >&2; exit 1; }
+[ "$id" ] || { echo "Error: Failed to get release id for latest"; echo "$response\n" >&2; exit 1; }
 
 # Upload the asset
 GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"

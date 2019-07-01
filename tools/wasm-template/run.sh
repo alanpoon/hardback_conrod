4f0c91a67759a0014b1b65fcd7c0888c010ef5d6
alanpoon <brightshadowx@gmail.com>
2019-07-01 23:01:18 +0800

    test sscache

--- tools/wasm-template/run.sh
+++ tools/wasm-template/run.sh
@@ -1,12 +1,11 @@
 SCRIPT_DIR=$(dirname "$0")
-BASE_DIR=$SCRIPT_DIR/../../../../
+BASE_DIR=$SCRIPT_DIR/../..
 
 set -e
-xargo build --example $1 --target wasm32-unknown-unknown
+cargo build --bin hardback --target wasm32-unknown-unknown
 rm -rf $SCRIPT_DIR/dist
 mkdir $SCRIPT_DIR/dist
 mkdir $SCRIPT_DIR/dist/intermediate
-cp $BASE_DIR/target/wasm32-unknown-unknown/debug/examples/$1.wasm $SCRIPT_DIR/dist/intermediate/native.wasm
-wasm-bindgen $SCRIPT_DIR/dist/intermediate/native.wasm --out-dir $SCRIPT_DIR/dist --no-typescript
-
+cp $BASE_DIR/target/wasm32-unknown-unknown/debug/hardback.wasm $SCRIPT_DIR/dist/intermediate/native.wasm
+wasm-bindgen --target web $SCRIPT_DIR/dist/intermediate/native.wasm --out-dir $SCRIPT_DIR/dist --no-typescript
 cd $SCRIPT_DIR
